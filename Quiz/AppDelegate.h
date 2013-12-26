43249f88d0a109a23f5da297ddeffa8e12dca189
Socoboy <socoboy@Mac-Pro-cua-MacPc.local>
2013-12-26 23:34:51 +0700

    init

---
+++ Quiz/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Socoboy on 12/26/13.
+//  Copyright (c) 2013 Socoboy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by Socoboy on 12/26/13.
+//  Copyright (c) 2013 Socoboy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
